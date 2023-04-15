40b44e8b6f3586da7db4e2009f4dc73478278d1d
pantawatthunanukul <pantawatdev88@gmail.com>
2023-04-16 01:49:10 +0700

    edit SettingPage() --> เด้งหน้าไปกลับระหว่างหน้า ProfileScreen โดยมี Nav

--- app_aceso/lib/pages/profile/settingpage.dart
+++ app_aceso/lib/pages/profile/settingpage.dart
@@ -1,17 +1,56 @@
+import 'package:app_aceso/pages/profile.dart';
 import 'package:flutter/material.dart';
+import '../../constants.dart';
+
 
 class SettingPage extends StatelessWidget {
   //const SettingPage({super.key});
   //const SettingPage({Key? key}) : super(key: key);
   const SettingPage({Key? key});
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: const <Widget>[Text('tesssst')],
-      )),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: <Widget>[
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: IconButton(
+                        icon: Iback,
+                        color: APrimaryColor,
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (BuildContext context) => ProfilePage()),
+                          );
+                        },
+                      ),
+                    ),
+                    const SizedBox(width: 100),
+                    Expanded(
+                      child: Text(
+                        'การตั้งค่า',
+                        style: textHeading,
+                      ),
+                    ),
+
+
+                  ],
+                ),
+                const SizedBox(height: 20),
+              ],
+            ),
+          )
+      ),
     );
   }
 }
