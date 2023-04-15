e06952ae339a73b366bcd25c2cddf7ec6f39a0d2
pantawatthunanukul <pantawatdev88@gmail.com>
2023-04-16 01:31:43 +0700

    edit ReadPrivacy()

--- app_aceso/lib/pages/profile/settingpage.dart
+++ app_aceso/lib/pages/profile/settingpage.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 
 class SettingPage extends StatelessWidget {
   //const SettingPage({super.key});
-  const SettingPage({Key? key}) : super(key: key);
+  //const SettingPage({Key? key}) : super(key: key);
+  const SettingPage({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
           child: Column(
-        children: const <Widget>[Text('test')],
+        children: const <Widget>[Text('tesssst')],
       )),
     );
   }
