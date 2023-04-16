38d470eaef92a4a8aa6bd214a4136d5b10cd4668
pantawatthunanukul <pantawatdev88@gmail.com>
2023-04-17 01:10:39 +0700

    Add setting_profilepage()

---
+++ app_aceso/lib/pages/profile/setting_profilepage.dart
@@ -0,0 +1,267 @@
+import 'package:app_aceso/pages/login.dart';
+import 'package:app_aceso/pages/profile.dart';
+import 'package:app_aceso/pages/profile/edit_profile.dart';
+import 'package:app_aceso/pages/profile/read_privacy_agreement.dart';
+import 'package:app_aceso/pages/profile/settingpage.dart';
+import 'package:flutter/material.dart';
+import '../../constants.dart';
+import '../widget/widget_button.dart';
+
+
+class SettingProfilepage extends StatelessWidget {
+  //const SettingPage({super.key});
+  //const SettingPage({Key? key}) : super(key: key);
+  const SettingProfilepage({Key? key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    final String email = '63015123@kmitl.ac.th';
+    final String Fullname = 'พันธวัช ตันอนุกูล';
+    final String Nickname = 'เล้ง';
+    final String Faculty = 'วิศวกรรมศาสตร์';
+    final String Birthday = '18 เมษายน 2000';
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: <Widget>[
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: IconButton(
+                        icon: Iback,
+                        color: APrimaryColor,
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const SettingPage()),
+                          );
+
+                        },
+                      ),
+                    ),
+                    const SizedBox(width: 100),
+                    const Expanded(
+                      child: Text(
+                        'ข้อมูลส่วนตัว',
+                        style: textHeading,
+                      ),
+                    ),
+
+
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    children: <Widget>[
+                      //--------------------------------อีเมล--------------------------------
+                      SizedBox(
+                        child:Row(
+                          children: <Widget>[
+
+                            Expanded(
+                              child: Row(
+                                  children: <Widget>[
+                                    SizedBox(width: 20,),
+                                    Text('อีเมล',style: textnormalbold,),
+                                    SizedBox(width: 42,),
+                                    Text(email, style: textnormalLight,),
+                                  ],
+                                ),
+                            )
+
+                          ],
+                        ),
+                      ),
+
+                      SizedBox(height: 5),
+
+                      //--------------------------------อีเมล--------------------------------
+                      //--------------------------------เส้นแบ่ง--------------------------------
+                      Row(
+                        children: [
+                      SizedBox(height: 10,width: 95,),
+                          Expanded(child: SizedBox(height: 0.5,width: 0.5,child: Container(color: Color(0xff9BA3EB),
+                          ),
+                          )
+                          ),
+                        ],
+                      ),
+                      //--------------------------------เส้นแบ่ง--------------------------------
+                      //--------------------------------ชื่อ-สกุล--------------------------------
+                      SizedBox(
+                        child:Row(
+                          children: <Widget>[
+                            Expanded(
+                            child: Row(
+                              children: <Widget>[
+                                SizedBox(height: 40,),
+                                SizedBox(width: 20,),
+                                Text('ชื่อ-สกุล',style: textnormalbold,),
+                                SizedBox(width: 20,),
+                                Text(Fullname, style: textnormal,),
+                              ],
+                            ),
+                          )
+
+                          ],
+                        ),
+                      ),
+                      //--------------------------------ชื่อ-สกุล--------------------------------
+                      //--------------------------------เส้นแบ่ง--------------------------------
+                      Row(
+                        children: [
+                          SizedBox(height: 10,width: 95,),
+                          Expanded(child: SizedBox(height: 0.5,width: 0.5,child: Container(color: Color(0xff9BA3EB),),)),
+                        ],
+                      ),
+                      //--------------------------------เส้นแบ่ง--------------------------------
+                      //--------------------------------ชื่อเล่น--------------------------------
+                      SizedBox(
+                        child:Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: Row(
+                                children: <Widget>[
+                                  SizedBox(height: 40,),
+                                  SizedBox(width: 20,),
+                                  Text('ชื่อเล่น',style: textnormalbold,),
+                                  SizedBox(width: 33,),
+                                  Text(Nickname, style: textnormal,),
+                                ],
+                              ),
+                            )
+
+                          ],
+                        ),
+                      ),
+                      //--------------------------------ชื่อเล่น--------------------------------
+                      //--------------------------------เส้นแบ่ง--------------------------------
+                      Row(
+                        children: [
+                          SizedBox(height: 10,width: 95,),
+                          Expanded(child: SizedBox(height: 0.5,width: 0.5,child: Container(color: Color(0xff9BA3EB),),)),
+                        ],
+                      ),
+                      //--------------------------------เส้นแบ่ง--------------------------------
+                      //--------------------------------คณะ--------------------------------
+                      SizedBox(
+                        child:Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: Row(
+                                children: <Widget>[
+                                  SizedBox(height: 40,),
+                                  SizedBox(width: 20,),
+                                  Text('คณะ',style: textnormalbold,),
+                                  SizedBox(width: 45,),
+                                  Text(Faculty, style: textnormal,),
+                                ],
+                              ),
+                            )
+
+                          ],
+                        ),
+                      ),
+                      //--------------------------------คณะ--------------------------------
+                      //--------------------------------เส้นแบ่ง--------------------------------
+                      Row(
+                        children: [
+                          SizedBox(height: 10,width: 95,),
+                          Expanded(child: SizedBox(height: 0.5,width: 0.5,child: Container(color: Color(0xff9BA3EB),),)),
+                        ],
+                      ),
+                      //--------------------------------เส้นแบ่ง--------------------------------
+                      //--------------------------------วันเกิด--------------------------------
+                      SizedBox(
+                        child:Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: Row(
+                                children: <Widget>[
+                                  SizedBox(height: 40,),
+                                  SizedBox(width: 20,),
+                                  Text('วันเกิด',style: textnormalbold,),
+                                  SizedBox(width: 33,),
+                                  Text(Birthday, style: textnormal,),
+                                ],
+                              ),
+                            )
+
+                          ],
+                        ),
+                      ),
+                      //--------------------------------วันเกิด--------------------------------
+                      //--------------------------------เส้นแบ่ง--------------------------------
+                      Row(
+                        children: [
+                          SizedBox(height: 10,width: 95,),
+                          Expanded(child: SizedBox(height: 0.5,width: 0.5,child: Container(color: Color(0xff9BA3EB),),)),
+                        ],
+                      ),
+                      //--------------------------------เส้นแบ่ง--------------------------------
+                      SizedBox(height: 420,width: 1,),
+
+                      //--------------------------------ออกจากระบบ--------------------------------
+                      SizedBox(
+                        child:Row(
+                          children: <Widget>[
+
+                            //Image.asset('assets/person.png', height: 40, width: 40),
+                            Expanded(child: ElevatedButton(
+                              child: const ButtonOperation(BTname: 'แก้ไขข้อมูลส่วนตัว'),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (BuildContext context) => const EditProfile()),
+                                );
+                              },
+                            ),
+                            ),
+
+                          ],
+                        ),
+                      ),
+
+                      //--------------------------------ออกจากระบบ--------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+      ),
+    );
+  }
+
+
+}
+
+
+
