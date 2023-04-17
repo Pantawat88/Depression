994d8441855589862375195113ad4d78b6caff58
pantawatthunanukul <pantawatdev88@gmail.com>
2023-04-18 02:47:50 +0700

    Edit success_register.dart.dart
    -*-> Add Nickname รอดึงค่าจากหน้ากรอกข้อมูลตอนลงทะเบียน
    -*-> Add เพิ่มหน้า create_avatar.dart เพื่อดึงตัวแปร Avatar
    
    --> Edit คอมเม้นหน้า edit_avatar.dart
    --> Add เพิ่มหน้า home.dart
    --> Add เพิ่มหน้า constants.dart
    --> Add เพิ่มหน้า widget_button.dart

--- app_aceso/lib/pages/register/success_register.dart
+++ app_aceso/lib/pages/register/success_register.dart
@@ -1,13 +1,154 @@
 import 'package:app_aceso/background.dart';
+import 'package:app_aceso/pages/home.dart';
+//import 'package:app_aceso/pages/profile/edit_avatar.dart';
+//import 'package:app_aceso/pages/profile/edit_avatar.dart';
 import 'package:flutter/material.dart';
+import 'package:app_aceso/constants.dart';
+import '../widget/widget_button.dart';
+import 'package:app_aceso/pages/register/personal_information.dart';
 
-class SuccessRegister extends StatelessWidget {
-  const SuccessRegister({super.key});
+
+import 'package:app_aceso/pages/register/create_avatar.dart';
+
+
+class SuccessRegister extends StatefulWidget {
+  final selectedName;
+  //const SuccessRegister({super.key});
+  const SuccessRegister ({Key? key, this.selectedName}) : super(key: key);
+
+  @override
+  State<SuccessRegister> createState() => _SuccessRegister();
+}
+
+
+
+class _SuccessRegister extends State<SuccessRegister> {
+
+  String Nickname = 'เล้ง';
 
   @override
   Widget build(BuildContext context) {
-    return Background(
-      child: Container(),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.topLeft,
+              margin: const EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(height: 60,),
+                  Center(
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Text(
+                            'ลงทะเบียน',
+                            style: textHeading,
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Center(
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Text(
+                            'เสร็จเรียบร้อยแล้ว',
+                            style: textHeadingOrange,
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+
+
+                ],
+              ),
+            ),
+            SizedBox(height: 30),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Container(
+                    width: 260,
+                    height: 270,
+
+                    child: FittedBox(
+                      fit: BoxFit.contain,
+                      child:
+                      selectedName == "มาโต้"
+                          ? Image.asset('assets/avatar/mato/mato3.gif')
+                          : selectedName == "เนเน่"
+                          ? Image.asset('assets/avatar/nene/nene2.gif')
+                          : selectedName == "หูหนู"
+                          ? Image.asset('assets/avatar/huhnu/huhnu4.gif')
+                          : selectedName == "เนโกะ"
+                          ? Image.asset('assets/avatar/neko/neko2.gif')
+                          : selectedName == "ราเม็ง"
+                          ? Image.asset('assets/avatar/ramen/ramen2.gif')
+                          : selectedName == "โชน"
+                          ? Image.asset('assets/avatar/chon/chon3.gif')
+                          : null,
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      SizedBox(height: 100,width: 300,),
+                            Text(
+                              'ยินดีต้อนรับ',
+                              style: textnormalBig,
+                            ),
+                            Text(
+                              '$Nickname',
+                              style: textnormalBigOrange,
+                            ),
+                      Text(
+                        'เข้าสู่ Aceso มาเช็คสุขภาพใจ\nไปพร้อมกับมาโต้กันเถอะ',
+                        style: textnormalBig,
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                   //SizedBox(height: 10),
+
+                ],
+              ),
+            ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              margin: const EdgeInsets.all(defaultPadding),
+              child: ElevatedButton(
+                child: const ButtonOperation(BTname: 'เริ่มต้นใช้งาน'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) =>
+                        const HomePage()),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
+
 }
+
+
+
+
+
