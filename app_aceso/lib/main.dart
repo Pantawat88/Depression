3f354fc235904cff93afb80fef0405ee608c2554
Nirmon29 <niramon.fn@gmail.com>
2023-04-19 03:16:17 +0700

    ีupdate

--- app_aceso/lib/main.dart
+++ app_aceso/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:app_aceso/constants.dart';
 import 'package:app_aceso/pages/login.dart';
 import 'package:flutter/material.dart';
-//import 'package:flutter_launcher_icons/xml_templates.dart';
 
 void main() => runApp(const MyApp());
 
